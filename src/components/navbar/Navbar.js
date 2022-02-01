41b432667930f57c456d0bb4917acc5e1e08608f
edmarsht <edtoulet@gmail.com>
2022-02-01 12:23:04 +0100

    remove githublogo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Navbar.css";
-import githubicon from "../../assets/githubicon.svg";
 
 function Navbar() {
   return (
@@ -10,9 +9,6 @@ function Navbar() {
       data-aos-duration="1300"
     >
       <p>Call me. (+33) 06 27 13 57 23</p>
-      <div className="portfolio__navbar-logo pulsate-fwd">
-        <a href="https://github.com/edmarsht" target="_blank"><img src={githubicon} alt="Github Edouard Toulet"/></a>
-      </div>
     </div>
   );
 }
